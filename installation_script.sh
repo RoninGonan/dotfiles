7e55a2cd23089b3506d448398eacfcb53e00055b
Antonio Gonzalez <antonygzz.3@gmail.com>
2024-06-25 23:28:47 -0700

    Updating installation_script

--- installation_script.sh
+++ installation_script.sh
@@ -2,14 +2,24 @@
 
 sudo apt install nala
 sudo nala install kitty
-sudo nala install fzf
-sudo nala install neofetch
+
+git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
+~/.fzf/install
+
 sudo nala install bat
 sudo nala install tldr
 sudo nala install thefuck
 sudo nala install starship
 sudo nala intall btop
 sudo nala install cmatrix
+
+wget -qO - https://gitlab.com/paulcarroty/vscodium-deb-rpm-repo/raw/master/pub.gpg \
+    | gpg --dearmor \
+    | sudo dd of=/usr/share/keyrings/vscodium-archive-keyring.gpg
+
+echo 'deb [ signed-by=/usr/share/keyrings/vscodium-archive-keyring.gpg ] https://download.vscodium.com/debs vscodium main' \
+    | sudo tee /etc/apt/sources.list.d/vscodium.list
+sudo nala update
 sudo nala install codium
 sudo nala install godotsharp
 sudo nala install nvim
